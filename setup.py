95f0bfbf8398c5da9777abda1b72c7f86310bebf
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2015-09-12 11:55:22 -0400

    indentation in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
-setup(name='gae_pylint_plugin',
-      version='0.1',
-      py_modules=['gae_pylint_plugin'])
+setup(
+  name='gae_pylint_plugin',
+  version='0.1',
+  py_modules=['gae_pylint_plugin']
+)
